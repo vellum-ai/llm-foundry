110fe2b89b66bdef4aedd18ba21c0537ad65647c
siddseethepalli <siddseethepalli@gmail.com>
2023-10-10 14:23:30 -0700

    fix paths

--- llmfoundry/callbacks/save_peft_callback.py
+++ llmfoundry/callbacks/save_peft_callback.py
@@ -14,17 +14,14 @@ class SavePeftCallback(Callback):
         super().__init__(**kwargs)
 
     def epoch_checkpoint(self, state, logger):
-        save_folder = f"adapter_{state.timestamp.epoch.value}"
-        peft_model = state.model.model
+        save_folder = f"adapter_ep{state.timestamp.epoch.value}"
 
+        peft_model = state.model.model
         peft_model.save_pretrained(save_folder)
 
-        # Iterate over all the files in the folder
         for filename in os.listdir(save_folder):
-            # Create full filepath
-            filepath = os.path.join(save_folder, filename)
-            # Upload the file to GCS
-            self.store.upload_object(filepath, filename)
+            path = os.path.join(save_folder, filename)
+            self.store.upload_object(path, path)
 
         # Remove the folder
         shutil.rmtree(save_folder)
